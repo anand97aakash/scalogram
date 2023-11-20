552af7cc6e1c16720935c8bf6a3d4111c8486551
Akash Anand <anand97aakash@gmail.com>
2023-11-20 04:17:58 -0600

    scalogram_extract_by_extent.R

--- R/scalogram_extract_by_extent.R
+++ R/scalogram_extract_by_extent.R
@@ -1,42 +1,82 @@
 scalogram.extract_by_extent <- function(raster_file, shapefile_file, x) {
   library(raster)
   library(sf)
+  library(sp)
+  library(rgdal)
   library(progress)
-
+  
   # Load the shapefile
   shapefile <- st_read(shapefile_file)
 
-  # Load the CumDHI raster
-  CumDHI <- raster(raster_file)
-
-  # Check if CumDHI has valid values
+    # Load the raster
+  data <- raster(raster_file)
+  
+  # Check if raster has valid values
   if (!hasValues(CumDHI)) {
     stop("CumDHI raster does not have any valid values.")
   }
-
-  # Create an empty list to store the results
-  results_list <- list()
-
-  # Create a progress bar
-  pb <- progress_bar$new(total = length(x), width = 60)
-
-  # Iterate over the values of x
-  for (i in seq_along(x)) {
-    nrow <- x[i]
-
-    # Perform reduce neighborhood operation with mean reducer and specified kernel size
-    L8_CumDHI_mean <- focal(CumDHI, w = matrix(1, nrow = nrow, ncol = nrow), fun = mean)
-
-    # Extract the values to the shapefile
-    L8_CumDHI_mean_extract <- extract(L8_CumDHI_mean, shapefile, fun = mean, na.rm = TRUE)
-
-    # Store the results in the list
-    results_list[[paste0("Extent_", nrow,"x",nrow)]] <- L8_CumDHI_mean_extract
-
-    # Update the progress bar
-    pb$tick()
+  
+  ###If the coordinate system of raster and shapefile is different then this code will help in matching them
+  # Check current coordinate systems
+  shapefile_crs <- st_crs(shapefile)
+  raster_crs <- crs(CumDHI)
+  
+  # If CRS doesn't match, reproject the shapefile to match the raster
+  if (!identical(st_crs(shapefile), raster_crs)) {
+    shapefile <- st_transform(shapefile, crs = raster_crs)
+  }  
+  
+    # Vector of extents
+  radii <- x    
+  
+  #############################################################################################
+  # Initialize an empty data frame to store the results
+  result_df <- data.frame(ID = shapefile$id, Lat = shapefile$lat, Lon = shapefile$lon, stringsAsFactors = FALSE)
+  # Extract the plot ID information
+  ID <- shapefile$id
+  
+  # Loop over each radius
+  for (i in seq_along(radii)) {
+    radius <- radii[i]
+    
+    # define the plot edges based upon the plot radius
+    yPlus <- shapefile$lat + radius*res(data)[1]
+    xPlus <- shapefile$lon + radius*res(data)[1]
+    yMinus <- shapefile$lat - radius*res(data)[1]
+    xMinus <- shapefile$lon - radius*res(data)[1]
+    
+    # calculate polygon coordinates for each plot centroid
+    square <- cbind(xMinus, yPlus,  # NW corner
+                    xPlus, yPlus,  # NE corner
+                    xPlus, yMinus,  # SE corner
+                    xMinus, yMinus, # SW corner
+                    xMinus, yPlus)  # NW corner again - close polygon
+    
+    # Extract the plot ID information
+    ID <- shapefile$id
+    
+    # Create spatial polygons from coordinates
+    polys <- SpatialPolygons(
+      mapply(
+        function(poly, id) {
+          xy <- matrix(poly, ncol = 2, byrow = TRUE)
+          Polygons(list(Polygon(xy)), ID = id)
+        },
+        split(square, row(square)),
+        ID
+      ),
+      proj4string = CRS(as.character("+proj=longlat +datum=WGS84 +no_defs"))
+    )
+    
+    # Extract the raster values for each polygon
+    result <- extract(data, polys, fun = mean, na.rm = TRUE)
+    
+    # Add the result as a new column with a dynamic column name
+    col_name <- paste("Extract_", radii[i],"x",radii[i], sep = "")
+    #result_df[[col_name]] <- result
+    result_df[[col_name]] <- as.vector(result)
   }
-
   # Return the results as a data frame
-  return(data.frame(results_list))
+  return(data.frame(result_df))
 }
+  
